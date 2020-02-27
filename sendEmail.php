a45755d7c7df3df277854f60f0f4acb681fbf094
Bosko <boskoskoric@hotmail.com>
2020-02-27 19:39:20 +0100

    fixing comments spaces

--- sendEmail.php
+++ sendEmail.php
@@ -40,7 +40,7 @@ try {
         );
 
 
-    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->SMTPDebug = 0;                                   // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
